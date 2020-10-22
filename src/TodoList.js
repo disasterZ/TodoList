faf1c50a03edac91d81c9c47bc568be571164936
disasterZ <937934696@qq.com>
2020-10-22 16:59:35 +0800

    未拆分（3.5前）

---
+++ src/TodoList.js
@@ -0,0 +1,69 @@
+import React, { Component , Fragment } from 'react';
+import './style.css';
+
+class TodoList extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            inputValue:'',
+            list:[],
+        }
+    } 
+
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    {/*这是一条注释*/}
+                    <label htmlFor="insert">输入内容</label>
+                    <input 
+                        id='insert'
+                        className='input'
+                        value={this.state.inputValue} 
+                        onChange={this.handleInputChange.bind(this)}
+                    />
+                    <button onClick={this.handleBtnClick.bind(this)}>
+                        提交
+                    </button>
+                </div>
+                <ul>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return (
+                                    <li     
+                                    key={index} 
+                                    onClick={this.handleDelete.bind(this,index)}
+                                    dangerouslySetInnerHTML={{__html:item}}
+                                    >
+                                    </li>
+                                ) 
+                        })
+                    }
+                </ul>
+            </Fragment>
+        )
+    }
+
+    handleInputChange(e){
+        this.setState({
+            inputValue:e.target.value
+        })
+    }
+
+    handleBtnClick(){
+        this.setState({
+            list:[...this.state.list,this.state.inputValue],
+            inputValue:'',
+        })
+    }
+
+    handleDelete(index){
+        const list=[...this.state.list];
+        list.splice(index,1);
+        this.setState({
+            list:list,
+        })
+    }
+}
+export default TodoList;
