aef3fc02ec061ec8229371e989be12d4118a1486
disasterZ <937934696@qq.com>
2020-10-26 15:43:06 +0800

    stateless components

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,35 +1,34 @@
 import React,{Component,Fragment} from 'react';
 import {Input,Button,List} from 'antd';
 
-class TodoListUI extends Component{
-    render() {
-        return (
-          <Fragment>
-            <div style={{ marginTop: '10px', marginLeft: '10px' }}>
-              <Input
-                value={this.props.inputValue}
-                placeholder="todo info"
-                style={{ width: '300px' }}
-                onChange={this.props.handleInputChange}
-              />
-              <Button
-                type='primary'
-                style={{ marginLeft: '10px' }}
-                onClick={this.props.handleBtnClick}
-              >提交</Button>
-            </div>
-            <List
-              style={{ marginTop:10,marginLeft: '10px', width: '300px' }}
-              bordered
-              dataSource={this.props.List}
-              renderItem={(item,index) => (
-                <List.Item onClick={this.props.handleItemClick.bind(this,index)}>
-                  {item}
-                </List.Item>
-              )}
+const TodoListUI = (props)=>{
+    return (
+        <Fragment>
+          <div style={{ marginTop: '10px', marginLeft: '10px' }}>
+            <Input
+              value={props.inputValue}
+              placeholder="todo info"
+              style={{ width: '300px' }}
+              onChange={props.handleInputChange}
             />
-          </Fragment>
-        )
-      }
+            <Button
+              type='primary'
+              style={{ marginLeft: '10px' }}
+              onClick={props.handleBtnClick}
+            >提交</Button>
+          </div>
+          <List
+            style={{ marginTop:10,marginLeft: '10px', width: '300px' }}
+            bordered
+            dataSource={props.List}
+            renderItem={(item,index) => (
+              <List.Item onClick={props.handleItemClick.bind(this,index)}>
+                {item}
+              </List.Item>
+            )}
+          />
+        </Fragment>
+      )
 }
+
 export default TodoListUI;
